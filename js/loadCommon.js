59462f2237b96126d2ecfa67a8ea46822a0b8695
Tianyu Liu <kingcrimsontianyu@gmail.com>
2020-03-25 11:40:14 -0400

    Handled href color.

--- js/loadCommon.js
+++ js/loadCommon.js
@@ -3,7 +3,31 @@
 { // start scope
 
 $(document).ready(() => {
-    $('#common-content').load('./common.html');
+    $('#common-content').load('./common.html', () => { // callback for load()
+
+        // get url query string
+        const urlParams = new URLSearchParams(window.location.search);
+        const myParam = urlParams.get('pageName');
+
+        // get element a
+        let myAList = document.querySelectorAll("#left-nav ul li a");
+
+        // reset
+        for(let idx = 0; idx < myAList.length; ++idx)
+        {
+            myAList[idx].classList.remove("manualActive");
+        }
+
+        // if the query string matches element a, add "manualActive" to a's class list
+        for(let idx = 0; idx < myAList.length; ++idx)
+        {
+            if(myAList[idx].id === myParam)
+            {
+                myAList[idx].classList.add("manualActive");
+                break;
+            }
+        }
+    });
 });
 
 } // end scope
