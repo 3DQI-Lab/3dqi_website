97bcce77fba7e8bc8175a6b9af18a6d6259a622f
ningchen8743 <ning.chen.cathy@gmail.com>
2020-04-03 18:19:27 -0400

    Added new member.

--- js/people.js
+++ js/people.js
@@ -35,6 +35,7 @@ window.addEventListener("load", () => {
     addAPerson("Tianyu Liu, Ph.D."  , "Research Fellow"   , "image/people/tianyu_liu.jpg");
     addAPerson("Qijun Shen, M.D."   , "Research Fellow"   , "image/people/qijun_shen.jpg");
     addAPerson("Guibo Luo, Ph.D."   , "Research Fellow"   , "image/people/guibo_luo.png");
+    addAPerson("Enhao Liu, B.S."    , "Research Fellow"   , "image/people/enhao_liu.png");
 
     let myDiv = document.querySelector("#current-members-div");
     let placeHolderDiv = document.createElement("div");
